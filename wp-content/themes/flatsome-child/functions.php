5cd1f10def40e32304d6117b22d0c09b047fa60d
thoanv <vanthoa225@gmail.com>
2023-06-21 11:15:14 +0700

    bo source

---
+++ wp-content/themes/flatsome-child/functions.php
@@ -0,0 +1,15 @@
+<?php
+function chowordpress_wc_custom_get_price_html( $price, $product ) {
+    if ( $product->get_price() == 0 ) {
+        if ( $product->is_on_sale() && $product->get_regular_price() ) {
+            $regular_price = wc_get_price_to_display( $product, array( 'qty' => 1, 'price' => $product->get_regular_price() ) );
+
+            $price = wc_format_price_range( $regular_price, __( 'Liên hệ', 'woocommerce' ) );
+        } else {
+            $price = '<span class="amount">' . __( 'Liên hệ', 'woocommerce' ) . '</span>';
+        }
+    }
+    return $price;
+}
+add_filter( 'woocommerce_get_price_html', 'chowordpress_wc_custom_get_price_html', 10, 2 );
+// Add custom Theme Functions here
